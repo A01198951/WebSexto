4ee85feb3a602848a1553b3eff98b556cda24d89
A01198951 <A01198951@tec.mx>
2025-03-06 22:49:47 -0600

    API

--- websexto/src/app/pages/informes/informes.component.ts
+++ websexto/src/app/pages/informes/informes.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Pokemon {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  abilities: {
+    ability: {
+      name: string;
+    };
+  }[];
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }[];
+}
 
 @Component({
   selector: 'app-informes',
-  imports: [],
   templateUrl: './informes.component.html',
-  styleUrl: './informes.component.css'
+  styleUrls: ['./informes.component.css']
 })
-export class InformesComponent {
+export class InformesComponent implements OnInit {
+  pokemons: Pokemon[] = [];
+  
+  constructor() { }
 
+  ngOnInit(): void {
+    this.fetchPokemonData('lucario');
+    this.fetchPokemonData('pikachu');
+    this.fetchPokemonData('charmander');
+  }
+
+  fetchPokemonData(pokemonName: string): void {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then(response => response.json())
+      .then(data => {
+        this.pokemons.push(data);
+      })
+      .catch(error => {
+        console.error(`Error fetching Pokemon ${pokemonName}:`, error);
+      });
+  }
+
+  getAbilityNames(abilities: any[]): string {
+    return abilities.map(ability => ability.ability.name).join(', ');
+  }
 }
